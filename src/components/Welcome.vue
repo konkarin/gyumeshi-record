4db1763fd5319247309b39ab5e613b9a0b0ed972
kon_karin <gd.lsd.168@gmail.com>
2020-02-23 21:49:43 +0900

    VuetifyとSASSの追加

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -10,17 +10,25 @@
       >
     </div>
     <div>
-      <input
-        type="password"
-        name
-        :value="password"
-        placeholder="パスワード"
-      >
+      <v-text-field
+        v-model="password"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]"
+        :type="showPassword ? 'text' : 'password'"
+        name="password"
+        label="パスワード"
+        hint="8文字以上"
+        counter
+        @click:append="showPassword = !showPassword"
+      />
     </div>
     <div>
       <button type="submit">
         登録する
       </button>
+      <!-- <v-btn type="submit">
+        登録する
+      </v-btn> -->
     </div>
     <div>
       アカウントをお持ちですか？
@@ -36,7 +44,12 @@ export default {
   data () {
     return {
       id: '',
-      password: ''
+      password: '',
+      showPassword: '',
+      rules: {
+        required: value => !!value || '8文字以上',
+        min: v => v.length >= 8 || '8文字以上'
+      }
     }
   }
 }
