264c3a25247aaa836ebdb0e8ced5885f6c0058a1
kon_karin <gd.lsd.168@gmail.com>
2020-02-27 02:02:16 +0900

    モーダル実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,35 +4,59 @@
       <modal
         name="tweet"
         height="auto"
+        width="300"
         :scrollable="true"
       >
-        <div
-          v-for="item in criteria"
-          :key="item"
-        >
-          <div>
-            {{ item.name }}
+        <form>
+          <div
+            style="padding: 20px;"
+          >
+            <template
+              v-for="item in criteria"
+            >
+              <div :key="item.name">
+                <v-text-field
+                  :label="item.name"
+                  name="score"
+                  placeholder="25"
+                  outlined
+                />
+                <v-text-field
+                  v-model="item.memo"
+                  label="メモ"
+                  name="memo"
+                  placeholder="おいしい"
+                  outlined
+                />
+              </div>
+            </template>
+            <v-file-input
+              accept="image/jpeg,image/png,image/webp,image/gif"
+              label="File input"
+              @change="getFileName"
+            />
+            <v-checkbox
+              v-model="tweetFlg"
+              name="tweet-flg"
+              label="ツイートする"
+            />
+
+            <v-btn
+              type="submit"
+              class="mx-2"
+              color="blue white--text"
+            >
+              記録する
+            </v-btn>
           </div>
-          <v-text-field
-            label="点数"
-            name="score"
-            placeholder="25"
-            outlined
-          />
-          <v-text-field
-            label="メモ"
-            name="memo"
-            placeholder="メモ"
-            outlined
-          />
-        </div>
+        </form>
       </modal>
       <v-btn
         class="mx-2"
         fab
         dark
         color="blue"
-        @click="addTweet"
+        @click="record"
       >
         <v-icon dark>
           mdi-plus
@@ -40,7 +64,7 @@
       </v-btn>
     </div>
     <div
-      v-for="item in tweetList"
+      v-for="item in timeline"
       :key="item"
       class="timeline"
     >
@@ -60,15 +84,25 @@ export default {
   data () {
     return {
       criteria: [],
-      tweetList: [0, 1, 2]
+      timeline: [0, 1, 2],
+      tweetFlg: false
     }
   },
   mounted () {
-    const list = [0, 1, 2]
+    const list = [
+      {
+        name: '肉',
+        memo: ''
+      },
+      {
+        name: '米',
+        memo: ''
+      }
+    ]
     this.criteria = list
   },
   methods: {
-    addTweet () {
+    record () {
       this.$modal.show('tweet')
     }
   }
