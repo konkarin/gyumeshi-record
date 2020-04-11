e654f91026b9a0cc8b0cc4f8be6b386af30a69d6
kon_karin <gd.lsd.168@gmail.com>
2020-04-11 16:48:58 +0900

    IEを〇す

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
       border-bottom-width: 1px;
       border-bottom-color: rgb(56, 68, 77);"
     >
-      Gyumeshi Record
+      <router-link to="/home">
+        Gyumeshi Record
+      </router-link>
     </h1>
     <div>
       <!-- TODO: 不透明オーバーレイつける -->
@@ -28,6 +30,15 @@ export default {
   data () {
     return {
       isLoading: true
+    }
+  },
+  created () {
+    // IEを〇す
+    const ua = navigator.userAgent.toLowerCase()
+    const isIE = (ua.indexOf('msie') >= 0 || ua.indexOf('trident') >= 0)
+
+    if (isIE) {
+      location.href = 'https://www.google.com/intl/ja_jp/chrome/'
     }
   },
   methods: {
